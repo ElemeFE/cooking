4ed4f46ab751af4ed80c9632862d140f48f8140c
HSunboy <nolanctt@hotmail.com>
2017-04-28 10:06:15 +0800

    Update is-installed.js

--- packages/cooking-cli/util/is-installed.js
+++ packages/cooking-cli/util/is-installed.js
@@ -1,11 +1,12 @@
 'use strict'
-const cp=require('child_process')
+const cp = require('child_process')
+
 module.exports = name => {
   try {
-    cp.execSync("node -e require.resolve('" + name + "')", { stdio: 'ignore' });
+    cp.execSync('node -e require.resolve("' + name + '")', {stdio: 'ignore'});
 
     return true
-  } catch (_) {
+  } catch (err) {
     return false
   }
 }
